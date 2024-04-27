1c4ce52862f6efb870a964fdb3a9ee3ef0c6bea9
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-04-27 22:15:53 +0530

    img fix

--- src/components/layout/Logo.jsx
+++ src/components/layout/Logo.jsx
@@ -5,7 +5,7 @@ function Logo() {
   return (
     <div className="logo block  h-[30px] w-[100px] mb-4">
       <NavLink to="/">
-        <img src="./src/assets/images/logo-no-background.svg" alt="logo" />
+        <img src="../src/assets/images/logo-no-background.svg" alt="logo" />
       </NavLink>
     </div>
   );
