aa755193825b3de917bcdc65909640d8a0194385
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-04-27 22:22:19 +0530

    rollback

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,7 +2,6 @@ import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { Analytics } from "@vercel/analytics/react";
 
 function Layout() {
   useGSAP(() => {
@@ -31,7 +30,6 @@ function Layout() {
         <br />
         <span className="tags top-tags all-tags">&lt;body&gt;</span>
         <Outlet />
-        <Analytics />
         <span className="tags bottom-tags all-tags body-bottom mb-3">
           &lt;/body&gt;
         </span>
