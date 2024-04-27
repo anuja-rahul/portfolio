b3ddb3f7f17308d09ab1ed89dd9df6a159279c9e
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-04-27 22:17:32 +0530

    img fix

--- src/components/layout/Logo.jsx
+++ src/components/layout/Logo.jsx
@@ -1,11 +1,12 @@
 import { NavLink } from "react-router-dom";
 import "./index.scss";
+import LogoImg from "../../assets/images/logo-no-background.svg"
 
 function Logo() {
   return (
     <div className="logo block  h-[30px] w-[100px] mb-4">
       <NavLink to="/">
-        <img src="../src/assets/images/logo-no-background.svg" alt="logo" />
+        <img src={LogoImg} alt="logo" />
       </NavLink>
     </div>
   );
