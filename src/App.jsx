bc816ce45b118c0b6076d265c815f858382a346a
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-05-01 20:37:37 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import {
-  Route,
-  Routes,
-  // createBrowserRouter,
-  // RouterProvider,
+  // Route,
+  // Routes,
+  createBrowserRouter,
+  RouterProvider,
 } from "react-router-dom";
 import "./App.scss";
 import Layout from "./components/layout/Layout";
@@ -12,61 +12,61 @@ import Projects from "./components/projects/Projects";
 import Contact from "./components/contact/Contact";
 
 function App() {
-  // const router = createBrowserRouter([
-  //   {
-  //     element: <Layout />,
-  //     children: [
-  //       {
-  //         path: "/",
-  //         element: <Home />,
-  //       },
-  //       // other pages....
-  //       {
-  //         path: "/about",
-  //         element: <About />,
-  //       },
-  //       {
-  //         path: "/projects",
-  //         element: <Projects />,
-  //       },
-  //       {
-  //         path: "/contacts",
-  //         element: <Contact />,
-  //       },
-  //       {
-  //         path: "*",
-  //         element: (
-  //           <div className="text-balance text-center text-7xl text-white">
-  //             Page not Found !
-  //           </div>
-  //         ),
-  //       },
-  //     ],
-  //   },
-  // ]);
+  const router = createBrowserRouter([
+    {
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        // other pages....
+        {
+          path: "/about",
+          element: <About />,
+        },
+        {
+          path: "/projects",
+          element: <Projects />,
+        },
+        {
+          path: "/contacts",
+          element: <Contact />,
+        },
+        {
+          path: "*",
+          element: (
+            <div className="text-balance text-center text-7xl text-white">
+              Page not Found !
+            </div>
+          ),
+        },
+      ],
+    },
+  ]);
 
-  // return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route
-            path="*"
-            element={
-              <div className="h-[30vh] text-balance text-center text-7xl text-white">
-                Page not Found !
-              </div>
-            }
-          />
-        </Route>
-      </Routes>
-    </>
-  );
+  // return (
+  //   <>
+  //     <Routes>
+  //       <Route path="/" element={<Layout />}>
+  //         <Route exact path="/" element={<Home />} />
+  //         <Route path="/about" element={<About />} />
+  //         <Route path="/projects" element={<Projects />} />
+  //         <Route path="/contact" element={<Contact />} />
+  //         <Route
+  //           path="*"
+  //           element={
+  //             <div className="h-[30vh] text-balance text-center text-7xl text-white">
+  //               Page not Found !
+  //             </div>
+  //           }
+  //         />
+  //       </Route>
+  //     </Routes>
+  //   </>
+  // );
 }
 
 export default App;
